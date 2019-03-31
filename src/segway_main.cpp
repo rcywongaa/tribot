742d11f4a06e9ff1edb8ad550916ce818fc9c4e9
Rufus Wong <rcywongaa@gmail.com>
2019-03-31 15:19:04 +0800

    [DIRTY] Attempt to add LQR controller to segway

--- src/segway_main.cpp
+++ src/segway_main.cpp
@@ -54,6 +54,28 @@ DEFINE_double(time_step, 0,
         "discrete updates and period equal to this time_step. "
         "If 0, the plant is modeled as a continuous system.");
 
+std::unique_ptr<systems::AffineSystem<double>> MakeBalancingLQRController(
+        MultibodyPlant<double>& plant, Context<double>& context)
+{
+    const int actuation_port_index = plant.get_actuation_input_port().get_index();
+    context.FixInputPort(actuation_port_index, Vector1d::Constant(0.0));
+    context.FixInputPort(
+            plant.get_applied_generalized_force_input_port().get_index(),
+            Eigen::VectorXd::Constant(7, 0.0));
+    printf("Plant q size: %d\n", plant.num_positions());
+    printf("Plant v size: %d\n", plant.num_velocities());
+
+    Eigen::Matrix4d Q = Eigen::Matrix4d::Identity();
+    Q(0, 0) = 10;
+    Q(1, 1) = 10;
+    Vector1d R = Vector1d::Constant(1);
+
+    return systems::controllers::LinearQuadraticRegulator(
+            plant, context, Q, R,
+            Eigen::Matrix<double, 0, 0>::Zero() /* No cross state/control costs */,
+            actuation_port_index);
+}
+
 int do_main() {
     systems::DiagramBuilder<double> builder;
 
@@ -95,22 +117,28 @@ int do_main() {
     // Sanity check on the availability of the optional source id before using it.
     DRAKE_DEMAND(plant.geometry_source_is_registered());
 
+    std::unique_ptr<systems::Context<double>> context = plant.CreateDefaultContext();
+
+    auto controller = builder.AddSystem(
+            MakeBalancingLQRController(plant, *context));
+    controller->set_name("controller");
+    builder.Connect(plant.get_continuous_state_output_port(),
+            controller->get_input_port());
+    builder.Connect(controller->get_output_port(),
+            plant.get_actuation_input_port());
+
     geometry::ConnectDrakeVisualizer(&builder, scene_graph);
     auto diagram = builder.Build();
 
-    std::unique_ptr<systems::Context<double>> diagram_context = diagram->CreateDefaultContext();
-    diagram->SetDefaultContext(diagram_context.get());
-    systems::Context<double>& plant_context = diagram->GetMutableSubsystemContext(plant, diagram_context.get());
-
-    plant_context.FixInputPort(plant.get_actuation_input_port().get_index(), Vector1d::Constant(50.0));
+    context->FixInputPort(plant.get_actuation_input_port().get_index(), Vector1d::Constant(50.0));
 
     // Get joints so that we can set initial conditions.
     const RevoluteJoint<double>& major_link_wheel_joint = plant.GetJointByName<RevoluteJoint>("major_link_wheel_joint");
 
     // Set initial state.
-    major_link_wheel_joint.set_angle(&plant_context, 0.5);
+    major_link_wheel_joint.set_angle(context.get(), 0.5);
 
-    systems::Simulator<double> simulator(*diagram, std::move(diagram_context));
+    systems::Simulator<double> simulator(*diagram);
 
     simulator.set_publish_every_time_step(false);
     simulator.set_target_realtime_rate(FLAGS_target_realtime_rate);
