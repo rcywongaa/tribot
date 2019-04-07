6749f63c109e783ca3db1195cfdf719e39d694a7
Rufus Wong <rcywongaa@gmail.com>
2019-04-07 23:06:21 +0800

    Create LQR controller for cart pole system

--- src/cart_pole_main.cpp
+++ src/cart_pole_main.cpp
@@ -48,15 +48,28 @@ DEFINE_double(time_step, 0,
         "If 0, the plant is modeled as a continuous system.");
 
 std::unique_ptr<systems::AffineSystem<double>> MakeBalancingLQRController(
-        MultibodyPlant<double>& plant, Context<double>& context)
+        MultibodyPlant<double>& plant)
 {
+    std::unique_ptr<Context<double>> context = plant.CreateDefaultContext();
     const int actuation_port_index = plant.get_actuation_input_port().get_index();
-    context.FixInputPort(actuation_port_index, Vector1d::Constant(0.0));
-    context.FixInputPort(
+    context->FixInputPort(actuation_port_index, Vector1d::Constant(0.0));
+    context->FixInputPort(
             plant.get_applied_generalized_force_input_port().get_index(),
-            Eigen::VectorXd::Constant(7, 0.0));
+            Eigen::Vector2d::Constant(0.0));
     printf("Plant q size: %d\n", plant.num_positions());
     printf("Plant v size: %d\n", plant.num_velocities());
+
+    // Get joints so that we can set initial conditions.
+    const PrismaticJoint<double>& cart_slider =
+        plant.GetJointByName<PrismaticJoint>("CartSlider");
+    const RevoluteJoint<double>& pole_pin =
+        plant.GetJointByName<RevoluteJoint>("PolePin");
+
+    // Set target state
+    cart_slider.set_translation(context.get(), 0.0);
+    cart_slider.set_translation_rate(context.get(), 0.0);
+    pole_pin.set_angle(context.get(), M_PI);
+    pole_pin.set_angular_rate(context.get(), 0.0);
 
     Eigen::Matrix4d Q = Eigen::Matrix4d::Identity();
     Q(0, 0) = 10;
@@ -64,7 +77,7 @@ std::unique_ptr<systems::AffineSystem<double>> MakeBalancingLQRController(
     Vector1d R = Vector1d::Constant(1);
 
     return systems::controllers::LinearQuadraticRegulator(
-            plant, context, Q, R,
+            plant, *context, Q, R,
             Eigen::Matrix<double, 0, 0>::Zero() /* No cross state/control costs */,
             actuation_port_index);
 }
@@ -77,22 +90,29 @@ int do_main() {
 
     // Make and add the cart_pole model.
     const std::string model_filename = getSrcDir() + "/../res/cart_pole.sdf";
-    MultibodyPlant<double>& cart_pole =
+    MultibodyPlant<double>& plant =
         *builder.AddSystem<MultibodyPlant>(FLAGS_time_step);
-    Parser(&cart_pole, &scene_graph).AddModelFromFile(model_filename);
+    Parser(&plant, &scene_graph).AddModelFromFile(model_filename);
 
     // Add gravity to the model.
-    cart_pole.AddForceElement<UniformGravityFieldElement>();
+    plant.AddForceElement<UniformGravityFieldElement>();
 
     // Now the model is complete.
-    cart_pole.Finalize();
+    plant.Finalize();
 
     // Sanity check on the availability of the optional source id before using it.
-    DRAKE_DEMAND(cart_pole.geometry_source_is_registered());
+    DRAKE_DEMAND(plant.geometry_source_is_registered());
+
+    auto controller = builder.AddSystem(MakeBalancingLQRController(plant));
+    controller->set_name("controller");
+    builder.Connect(plant.get_continuous_state_output_port(),
+            controller->get_input_port());
+    builder.Connect(controller->get_output_port(),
+            plant.get_actuation_input_port());
 
     builder.Connect(
-            cart_pole.get_geometry_poses_output_port(),
-            scene_graph.get_source_pose_port(cart_pole.get_source_id().value()));
+            plant.get_geometry_poses_output_port(),
+            scene_graph.get_source_pose_port(plant.get_source_id().value()));
 
     geometry::ConnectDrakeVisualizer(&builder, scene_graph);
     auto diagram = builder.Build();
@@ -101,22 +121,20 @@ int do_main() {
     std::unique_ptr<systems::Context<double>> diagram_context =
         diagram->CreateDefaultContext();
     diagram->SetDefaultContext(diagram_context.get());
-    systems::Context<double>& cart_pole_context =
-        diagram->GetMutableSubsystemContext(cart_pole, diagram_context.get());
-
-    // There is no input actuation in this example for the passive dynamics.
-    cart_pole_context.FixInputPort(
-            cart_pole.get_actuation_input_port().get_index(), Vector1d(0));
+    systems::Context<double>& context =
+        diagram->GetMutableSubsystemContext(plant, diagram_context.get());
 
     // Get joints so that we can set initial conditions.
     const PrismaticJoint<double>& cart_slider =
-        cart_pole.GetJointByName<PrismaticJoint>("CartSlider");
+        plant.GetJointByName<PrismaticJoint>("CartSlider");
     const RevoluteJoint<double>& pole_pin =
-        cart_pole.GetJointByName<RevoluteJoint>("PolePin");
+        plant.GetJointByName<RevoluteJoint>("PolePin");
 
-    // Set initial state.
-    cart_slider.set_translation(&cart_pole_context, 0.0);
-    pole_pin.set_angle(&cart_pole_context, M_PI);
+    // Set initial state
+    cart_slider.set_translation(&context, 0.0);
+    cart_slider.set_translation_rate(&context, 0.0);
+    pole_pin.set_angle(&context, M_PI+0.5);
+    pole_pin.set_angular_rate(&context, 0.0);
 
     systems::Simulator<double> simulator(*diagram, std::move(diagram_context));
 
