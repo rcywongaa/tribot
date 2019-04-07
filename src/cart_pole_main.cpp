6a31aed4f4d6b8110fa90e3d0a3fbdf634bd34d7
Rufus Wong <rcywongaa@gmail.com>
2019-04-07 16:30:11 +0800

    Retab and remove unused namespaces

--- src/cart_pole_main.cpp
+++ src/cart_pole_main.cpp
@@ -17,16 +17,10 @@
 #include "drake/systems/framework/diagram_builder.h"
 #include "drake/systems/controllers/linear_quadratic_regulator.h"
 
-namespace drake {
-namespace examples {
-namespace multibody {
-namespace cart_pole {
-namespace {
-
 using namespace drake;
 
 using geometry::SceneGraph;
-using lcm::DrakeLcm;
+using drake::lcm::DrakeLcm;
 
 // "multibody" namespace is ambiguous here without "drake::".
 using drake::multibody::MultibodyPlant;
@@ -42,16 +36,16 @@ inline const std::string getSrcDir()
 }
 
 DEFINE_double(target_realtime_rate, 1.0,
-              "Desired rate relative to real time.  See documentation for "
-              "Simulator::set_target_realtime_rate() for details.");
+        "Desired rate relative to real time.  See documentation for "
+        "Simulator::set_target_realtime_rate() for details.");
 
 DEFINE_double(simulation_time, 10.0,
-              "Desired duration of the simulation in seconds.");
+        "Desired duration of the simulation in seconds.");
 
 DEFINE_double(time_step, 0,
-            "If greater than zero, the plant is modeled as a system with "
-            "discrete updates and period equal to this time_step. "
-            "If 0, the plant is modeled as a continuous system.");
+        "If greater than zero, the plant is modeled as a system with "
+        "discrete updates and period equal to this time_step. "
+        "If 0, the plant is modeled as a continuous system.");
 
 std::unique_ptr<systems::AffineSystem<double>> MakeBalancingLQRController(
         MultibodyPlant<double>& plant, Context<double>& context)
@@ -76,76 +70,70 @@ std::unique_ptr<systems::AffineSystem<double>> MakeBalancingLQRController(
 }
 
 int do_main() {
-  systems::DiagramBuilder<double> builder;
+    systems::DiagramBuilder<double> builder;
 
-  SceneGraph<double>& scene_graph = *builder.AddSystem<SceneGraph>();
-  scene_graph.set_name("scene_graph");
+    SceneGraph<double>& scene_graph = *builder.AddSystem<SceneGraph>();
+    scene_graph.set_name("scene_graph");
 
-  // Make and add the cart_pole model.
-  const std::string model_filename = getSrcDir() + "/../res/cart_pole.sdf";
-  MultibodyPlant<double>& cart_pole =
-      *builder.AddSystem<MultibodyPlant>(FLAGS_time_step);
-  Parser(&cart_pole, &scene_graph).AddModelFromFile(model_filename);
+    // Make and add the cart_pole model.
+    const std::string model_filename = getSrcDir() + "/../res/cart_pole.sdf";
+    MultibodyPlant<double>& cart_pole =
+        *builder.AddSystem<MultibodyPlant>(FLAGS_time_step);
+    Parser(&cart_pole, &scene_graph).AddModelFromFile(model_filename);
 
-  // Add gravity to the model.
-  cart_pole.AddForceElement<UniformGravityFieldElement>();
+    // Add gravity to the model.
+    cart_pole.AddForceElement<UniformGravityFieldElement>();
 
-  // Now the model is complete.
-  cart_pole.Finalize();
+    // Now the model is complete.
+    cart_pole.Finalize();
 
-  // Sanity check on the availability of the optional source id before using it.
-  DRAKE_DEMAND(cart_pole.geometry_source_is_registered());
+    // Sanity check on the availability of the optional source id before using it.
+    DRAKE_DEMAND(cart_pole.geometry_source_is_registered());
 
-  builder.Connect(
-      cart_pole.get_geometry_poses_output_port(),
-      scene_graph.get_source_pose_port(cart_pole.get_source_id().value()));
+    builder.Connect(
+            cart_pole.get_geometry_poses_output_port(),
+            scene_graph.get_source_pose_port(cart_pole.get_source_id().value()));
 
-  geometry::ConnectDrakeVisualizer(&builder, scene_graph);
-  auto diagram = builder.Build();
+    geometry::ConnectDrakeVisualizer(&builder, scene_graph);
+    auto diagram = builder.Build();
 
-  // Create a context for this system:
-  std::unique_ptr<systems::Context<double>> diagram_context =
-      diagram->CreateDefaultContext();
-  diagram->SetDefaultContext(diagram_context.get());
-  systems::Context<double>& cart_pole_context =
-      diagram->GetMutableSubsystemContext(cart_pole, diagram_context.get());
+    // Create a context for this system:
+    std::unique_ptr<systems::Context<double>> diagram_context =
+        diagram->CreateDefaultContext();
+    diagram->SetDefaultContext(diagram_context.get());
+    systems::Context<double>& cart_pole_context =
+        diagram->GetMutableSubsystemContext(cart_pole, diagram_context.get());
 
-  // There is no input actuation in this example for the passive dynamics.
-  cart_pole_context.FixInputPort(
-      cart_pole.get_actuation_input_port().get_index(), Vector1d(0));
+    // There is no input actuation in this example for the passive dynamics.
+    cart_pole_context.FixInputPort(
+            cart_pole.get_actuation_input_port().get_index(), Vector1d(0));
 
-  // Get joints so that we can set initial conditions.
-  const PrismaticJoint<double>& cart_slider =
-      cart_pole.GetJointByName<PrismaticJoint>("CartSlider");
-  const RevoluteJoint<double>& pole_pin =
-      cart_pole.GetJointByName<RevoluteJoint>("PolePin");
+    // Get joints so that we can set initial conditions.
+    const PrismaticJoint<double>& cart_slider =
+        cart_pole.GetJointByName<PrismaticJoint>("CartSlider");
+    const RevoluteJoint<double>& pole_pin =
+        cart_pole.GetJointByName<RevoluteJoint>("PolePin");
 
-  // Set initial state.
-  cart_slider.set_translation(&cart_pole_context, 0.0);
-  pole_pin.set_angle(&cart_pole_context, M_PI);
+    // Set initial state.
+    cart_slider.set_translation(&cart_pole_context, 0.0);
+    pole_pin.set_angle(&cart_pole_context, M_PI);
 
-  systems::Simulator<double> simulator(*diagram, std::move(diagram_context));
+    systems::Simulator<double> simulator(*diagram, std::move(diagram_context));
 
-  simulator.set_publish_every_time_step(false);
-  simulator.set_target_realtime_rate(FLAGS_target_realtime_rate);
-  simulator.Initialize();
-  simulator.StepTo(FLAGS_simulation_time);
+    simulator.set_publish_every_time_step(false);
+    simulator.set_target_realtime_rate(FLAGS_target_realtime_rate);
+    simulator.Initialize();
+    simulator.StepTo(FLAGS_simulation_time);
 
-  return 0;
+    return 0;
 }
 
-}  // namespace
-}  // namespace cart_pole
-}  // namespace multibody
-}  // namespace examples
-}  // namespace drake
-
 int main(int argc, char* argv[]) {
-  gflags::SetUsageMessage(
-      "A simple cart pole demo using Drake's MultibodyPlant,"
-      "with SceneGraph visualization. "
-      "Launch drake-visualizer before running this example.");
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
-  drake::logging::HandleSpdlogGflags();
-  return drake::examples::multibody::cart_pole::do_main();
+    gflags::SetUsageMessage(
+            "A simple cart pole demo using Drake's MultibodyPlant,"
+            "with SceneGraph visualization. "
+            "Launch drake-visualizer before running this example.");
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    drake::logging::HandleSpdlogGflags();
+    return do_main();
 }
