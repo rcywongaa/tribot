87ddc7eca01e2bbefa179f25b12b19bf8ae5053e
Rufus Wong <rcywongaa@gmail.com>
2019-05-21 01:00:16 +0800

    Remove MobileInvertedPendulumPlant

--- src/mip_util.hpp
+++ src/mip_util.hpp
@@ -26,32 +26,3 @@ class MIPController : public drake::systems::LeafSystem<T>
         int input_idx;
         int output_idx;
 };
-
-template <typename T>
-class MobileInvertedPendulumPlant : public drake::systems::LeafSystem<T>
-{
-    public:
-        /* Required to solve "does not support ToAutoDiffXd"
-         * https://drake.mit.edu/doxygen_cxx/group__system__scalar__conversion.html
-         */
-        DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(MobileInvertedPendulumPlant);
-
-        /* Required to solve "does not support ToAutoDiffXd"
-         * https://drake.mit.edu/doxygen_cxx/group__system__scalar__conversion.html
-         */
-        template <typename U>
-            explicit MobileInvertedPendulumPlant(const MobileInvertedPendulumPlant<U>& other) : MobileInvertedPendulumPlant<T>() {}
-
-        MobileInvertedPendulumPlant();
-        const drake::systems::OutputPort<T>& get_state_output() const;
-        const drake::systems::InputPort<T>& get_torque_input() const;
-        void SetDefaultState(const drake::systems::Context<T>&, drake::systems::State<T>* state) const override;
-
-    private:
-        void DoCalcTimeDerivatives(
-                const drake::systems::Context<T>& context,
-                drake::systems::ContinuousState<T>* derivatives) const override;
-        void copyStateOut(const drake::systems::Context<T> &context, drake::systems::BasicVector<T> *output) const;
-        int state_port_idx;
-        int torque_port_idx;
-};
