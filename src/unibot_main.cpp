4f7c7a76c3e3bde8a245017afa3fe4b3537111a8
Rufus Wong <rcywongaa@gmail.com>
2019-05-23 04:29:26 +0800

    DIRTY unibot : Unibot spins when attempting to balance MIP

--- src/unibot_main.cpp
+++ src/unibot_main.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+
 #include <gflags/gflags.h>
 #include "drake/common/text_logging_gflags.h"
 #include "drake/multibody/plant/multibody_plant.h"
@@ -9,6 +11,7 @@
 #include "drake_util.hpp"
 #include "unibot_util.hpp"
 #include "acrobot_util.hpp"
+#include "mip_util.hpp"
 #include "StateConverter.hpp"
 
 using namespace Eigen;
@@ -29,10 +32,21 @@ void unibot_to_acrobot_state(const Eigen::VectorBlock<const VectorX<T>>& state, 
     output[3] = state[14]; // phi_dot
 }
 
+template <typename T>
+void unibot_to_mip_state(const Eigen::VectorBlock<const VectorX<T>>& state, Eigen::VectorBlock<VectorX<T>>& output)
+{
+    output[0] = state[5]; // theta (pitch of the rod)
+    //printf("pitch of rod = %f\n", output[0]);
+    output[1] = output[0] + state[7]; // psi (wheel angle) = pitch of rod + rodwheel angle
+    //printf("wheel angle = %f\n", output[1]);
+    output[2] = state[13]; // theta_dot
+    output[3] = output[2] + state[15]; // psi_dot
+}
+
 int main(int argc, char* argv[])
 {
     gflags::SetUsageMessage(
-            "A acrobot demo using Drake's MultibodyPlant,"
+            "A unibot demo using Drake's MultibodyPlant,"
             "with SceneGraph visualization. "
             "Launch drake-visualizer before running this example.");
     gflags::ParseCommandLineFlags(&argc, &argv, true);
@@ -46,21 +60,49 @@ int main(int argc, char* argv[])
     printf("plant num positions = %d\n", plant.num_positions());
     printf("plant num velocities = %d\n", plant.num_velocities());
 
-    ConversionFunc func(
+    ConversionFunc unibot_to_acrobot_func(
             unibot_to_acrobot_state<double>,
             unibot_to_acrobot_state<drake::AutoDiffXd>,
             unibot_to_acrobot_state<drake::symbolic::Expression>);
 
-    auto unibot_acrobot_converter = builder.AddSystem(std::make_unique<StateConverter<double>>(func, 16, 4));
+    ConversionFunc unibot_to_mip_func(
+            unibot_to_mip_state<double>,
+            unibot_to_mip_state<drake::AutoDiffXd>,
+            unibot_to_mip_state<drake::symbolic::Expression>);
+
+    auto unibot_acrobot_converter = builder.AddSystem(std::make_unique<StateConverter<double>>(unibot_to_acrobot_func, 16, 4));
     unibot_acrobot_converter->set_name("unibot_acrobot_converter");
+    auto unibot_mip_converter = builder.AddSystem(std::make_unique<StateConverter<double>>(unibot_to_mip_func, 16, 4));
+    unibot_mip_converter->set_name("unibot_mip_converter");
     auto acrobot_controller = builder.AddSystem(MakeAcrobotLQRController(getResDir() + "unibot_acrobot.sdf"));
     acrobot_controller->set_name("acrobot_controller");
+
+    // Must be consistent with mip.rsdf
+    const double m_r = 0.1; // rod mass
+    const double l_l = 0.5; // load_position
+    const double m_l = 0.4; // load mass
+    const double rod_length = 1.0; // rod length
+
+    MIPConfiguration cfg;
+    cfg.M_w = 0.2; // wheel mass
+    cfg.M_r = m_r + m_l; // total rod mass
+    cfg.R = 0.2; // wheel radius
+    cfg.L = rod_length/2.0; // half rod length
+    cfg.I_w = 0.5*cfg.M_w*cfg.R*cfg.R; // wheel inertia
+    cfg.I_r = m_r*rod_length*rod_length/3.0 + m_l*l_l*l_l; // rod inertia
+    auto mip_controller = builder.AddSystem(std::make_unique<MIPController<double>>(cfg, 0.0));
+    mip_controller->set_name("mip_controller");
+
     auto torque_converter = builder.AddSystem(std::make_unique<TorqueCombiner<double>>());
     torque_converter->set_name("torque_converter");
 
     builder.Connect(plant.get_state_output_port(), unibot_acrobot_converter->get_input_port());
     builder.Connect(unibot_acrobot_converter->get_output_port(), acrobot_controller->get_input_port());
     builder.Connect(acrobot_controller->get_output_port(), torque_converter->get_acrobot_input_port());
+
+    builder.Connect(plant.get_state_output_port(), unibot_mip_converter->get_input_port());
+    builder.Connect(unibot_mip_converter->get_output_port(), mip_controller->mip_state_input());
+    builder.Connect(mip_controller->torque_output(), torque_converter->get_mip_input_port());
 
     builder.Connect(torque_converter->get_torque_output_port(), plant.get_actuation_input_port());
 
@@ -69,20 +111,17 @@ int main(int argc, char* argv[])
     std::unique_ptr<systems::Context<double>> diagram_context = diagram->CreateDefaultContext();
 
     Context<double>& torque_converter_context = diagram->GetMutableSubsystemContext(*torque_converter, diagram_context.get());
-    torque_converter_context.FixInputPort(torque_converter->get_mip_input_port().get_index(), Vector1d::Zero());
+    //torque_converter_context.FixInputPort(torque_converter->get_mip_input_port().get_index(), Vector1d::Zero());
+    torque_converter_context.FixInputPort(torque_converter->get_acrobot_input_port().get_index(), Vector1d::Zero());
 
     // Create a plant_context for this system:
     Context<double>& plant_context = diagram->GetMutableSubsystemContext(plant, diagram_context.get());
 
-    // Get joints so that we can set initial conditions.
-    const RevoluteJoint<double>& phi = plant.GetJointByName<RevoluteJoint>("phi");
-    phi.set_angle(&plant_context, 0.20);
-
     // Sanity check to make sure unibot falls forward and sideways correctly
-    //const RevoluteJoint<double>& theta = plant.GetJointByName<RevoluteJoint>("theta");
-    //theta.set_angle(&plant_context, 0.30);
+    const RevoluteJoint<double>& theta = plant.GetJointByName<RevoluteJoint>("theta"); // pitch
+    theta.set_angle(&plant_context, 0.10*M_PI);
     //const RevoluteJoint<double>& roll = plant.GetJointByName<RevoluteJoint>("roll");
-    //roll.set_angle(&plant_context, 0.30);
+    //roll.set_angle(&plant_context, 0.02*M_PI);
     //const RevoluteJoint<double>& yaw = plant.GetJointByName<RevoluteJoint>("yaw");
     //yaw.set_angle(&plant_context, 0.30);
 
