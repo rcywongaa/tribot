5d9da0889c5c1ef7d1a4ec0654f91bee3b64618f
Rufus Wong <rcywongaa@gmail.com>
2019-05-25 03:00:22 +0800

    Use full state floating quaternion state for unibot_to_mip_state conversion Print state using Inspector

--- src/unibot_main.cpp
+++ src/unibot_main.cpp
@@ -13,6 +13,7 @@
 #include "acrobot_util.hpp"
 #include "mip_util.hpp"
 #include "StateConverter.hpp"
+#include "Inspector.hpp"
 
 using namespace Eigen;
 using namespace drake;
@@ -35,12 +36,40 @@ void unibot_to_acrobot_state(const Eigen::VectorBlock<const VectorX<T>>& state, 
 template <typename T>
 void unibot_to_mip_state(const Eigen::VectorBlock<const VectorX<T>>& state, Eigen::VectorBlock<VectorX<T>>& output)
 {
-    output[0] = state[5]; // theta (pitch of the rod)
+    drake::math::RollPitchYaw<T> rpy(Eigen::Quaternion<T>(state[0], state[1], state[2], state[3]));
+    output[0] = rpy.pitch_angle(); // theta (pitch of the rod)
     //printf("pitch of rod = %f\n", output[0]);
-    output[1] = output[0] + state[7]; // psi (wheel angle) = pitch of rod + rodwheel angle
+    output[1] = output[0] + state[8]; // psi (wheel angle) = pitch of rod + rodwheel angle
     //printf("wheel angle = %f\n", output[1]);
-    output[2] = state[13]; // theta_dot
-    output[3] = output[2] + state[15]; // psi_dot
+    output[2] = state[9]; // theta_dot
+    output[3] = output[2] + state[16]; // psi_dot
+}
+
+// state[0] = q1
+// state[1] = q2
+// state[2] = q3
+// state[3] = q4
+// state[4] = x
+// state[5] = y
+// state[6] = z
+// state[7] = phi
+// state[8] = wheel_joint
+// state[9] = roll_dot
+// state[12] = x_dot
+// state[13] = y_dot
+// state[14] = z_dot
+// state[15] = phi_dot
+// state[16] = wheel_dot
+template <typename T>
+void inspect_unibot(const Eigen::VectorBlock<const VectorX<T>>& state)
+{
+    printf("x = %f\n", state[4]);
+    printf("y = %f\n", state[5]);
+    printf("z = %f\n", state[6]);
+    //drake::math::RollPitchYaw<T> rpy(Eigen::Quaternion<T>(state[0], state[1], state[2], state[3]));
+    //printf("roll = %f\n", rpy.roll_angle());
+    //printf("pitch = %f\n", rpy.pitch_angle());
+    //printf("yaw = %f\n", rpy.yaw_angle());
 }
 
 int main(int argc, char* argv[])
@@ -69,13 +98,20 @@ int main(int argc, char* argv[])
             unibot_to_mip_state<double>,
             unibot_to_mip_state<drake::AutoDiffXd>,
             unibot_to_mip_state<drake::symbolic::Expression>);
-
     auto unibot_acrobot_converter = builder.AddSystem(std::make_unique<StateConverter<double>>(unibot_to_acrobot_func, 16, 4));
     unibot_acrobot_converter->set_name("unibot_acrobot_converter");
-    auto unibot_mip_converter = builder.AddSystem(std::make_unique<StateConverter<double>>(unibot_to_mip_func, 16, 4));
+    auto unibot_mip_converter = builder.AddSystem(std::make_unique<StateConverter<double>>(unibot_to_mip_func, 17, 4));
     unibot_mip_converter->set_name("unibot_mip_converter");
+
     auto acrobot_controller = builder.AddSystem(MakeAcrobotLQRController(getResDir() + "unibot_acrobot.sdf"));
     acrobot_controller->set_name("acrobot_controller");
+
+    InspectionFunc inspect_unibot_func(
+            inspect_unibot<double>,
+            inspect_unibot<drake::AutoDiffXd>,
+            inspect_unibot<drake::symbolic::Expression>);
+    auto unibot_inspector = builder.AddSystem(std::make_unique<Inspector<double>>(inspect_unibot_func, 17));
+    unibot_inspector->set_name("unibot_inspector");
 
     // Must be consistent with mip.rsdf
     const double m_r = 0.1; // rod mass
@@ -100,7 +136,8 @@ int main(int argc, char* argv[])
     builder.Connect(unibot_acrobot_converter->get_output_port(), acrobot_controller->get_input_port());
     builder.Connect(acrobot_controller->get_output_port(), torque_converter->get_acrobot_input_port());
 
-    builder.Connect(plant.get_state_output_port(), unibot_mip_converter->get_input_port());
+    builder.Connect(plant.get_state_output_port(), unibot_inspector->get_input_port());
+    builder.Connect(unibot_inspector->get_output_port(), unibot_mip_converter->get_input_port());
     builder.Connect(unibot_mip_converter->get_output_port(), mip_controller->mip_state_input());
     builder.Connect(mip_controller->torque_output(), torque_converter->get_mip_input_port());
 
