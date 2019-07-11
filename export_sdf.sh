d649505063f1505bc39e92e6cf153ccd7de4979c
Rufus Wong <rcywongaa@gmail.com>
2019-07-11 23:33:17 +0800

    Fix export_sdf.sh

--- export_sdf.sh
+++ export_sdf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+DIR=$(dirname "$(readlink -f "$0")")
 
-rsdf_files=$(find -name "*.rsdf")
+rsdf_files=$(find "$DIR" -name "*.rsdf")
 while read -r rsdf_file; do
     erb "${rsdf_file}" > "${rsdf_file%.*}".sdf
 done <<< "${rsdf_files}"
