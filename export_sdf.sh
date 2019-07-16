1ef5c19c50b80db3cca87650c45a1a5836a9acb7
Rufus Wong <rcywongaa@gmail.com>
2019-07-16 15:55:14 +0800

    Automatically run export_sdf.sh after each build

--- export_sdf.sh
+++ export_sdf.sh
@@ -3,5 +3,6 @@ DIR=$(dirname "$(readlink -f "$0")")
 
 rsdf_files=$(find "$DIR" -name "*.rsdf")
 while read -r rsdf_file; do
+    printf -- "Generating ${rsdf_file%.*}.sdf from ${rsdf_file}\n"
     erb "${rsdf_file}" > "${rsdf_file%.*}".sdf
 done <<< "${rsdf_files}"
